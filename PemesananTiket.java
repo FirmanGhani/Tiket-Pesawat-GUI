7bdb57ac52264cf04fddcec095b0ca58a2968417
FirmanGhani <firmanghani28@gmail.com>
2024-12-09 22:52:33 +0700

    Add files via upload

---
+++ PemesananTiket.java
@@ -0,0 +1,12 @@
+package com.mycompany.pemesanantiket;
+
+public class PemesananTiket {
+      
+    public static void main(String[] args) {
+        Tiket loginFrame = new Tiket();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
